1ae0c945b363dfb63c62a3763084c1455264c3d0
Moin-11 <Moin11.dev@gmail.com>
2020-09-09 14:55:24 +0530

    not gonna bother naming this commit

--- src/Material_Styles/dropDownMenuStyles.js
+++ src/Material_Styles/dropDownMenuStyles.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core";
 const dropDownStyles = makeStyles((theme) => ({
   formControl: {
     backgroundColor: "transparent",
-    margin: theme.spacing(2),
+    margin: theme.spacing(5),
     minWidth: 200,
   },
 }));
